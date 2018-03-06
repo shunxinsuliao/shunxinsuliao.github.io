798204a2bdd80f7e5cbd1b908fd9e124b8618693
dst <daishitong@xaut.edu.cn>
2018-03-06 12:37:11 +0800

    add product.

---
+++ js/bdtj.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?76e2077e8d2bb78d97d5077f5cbf8a2c";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
